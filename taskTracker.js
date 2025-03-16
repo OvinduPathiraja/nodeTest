b22760047d9ee2327cf95b5fd1f1917eb5e7f98a
DASANAYAKALAGE OVINDU THANUJ PATHIRAJA <cb010931@students.apiit.lk>
2025-03-16 08:09:23 +0530

    Auto-commit: 2025-03-16 08:09:23

--- taskTracker.js
+++ taskTracker.js
@@ -3,6 +3,6 @@ const readline = require('readline');
 
 const data = JSON.parse(fs.readFileSync(data.json, 'utf-8'));
 
-data.push({name:"ovindu", })
+data.push({name:"ovindu", age:""})
 
 fs.writeFileSync(filepath, "task" );
