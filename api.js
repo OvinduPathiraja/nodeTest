362448a5bfcce5c07acad0a95e8612a93e619291
DASANAYAKALAGE OVINDU THANUJ PATHIRAJA <cb010931@students.apiit.lk>
2025-03-28 07:32:29 +0530

    Auto-commit: 2025-03-28 07:32:29

--- api.js
+++ api.js
@@ -5,13 +5,20 @@ async function pushdata(key, value) {
     data[key] = { value};
 }
 
-pushdata("01","val")
-pushdata("02","val2")
+// pushdata("01","val")
+// pushdata("02","val2")
 
 Object.entries(data).forEach(([key,item]) => {
     console.log(key);
 })
 
-async function finddata(key){
-    
+async function finddata(val){
+    Object.entries(data).forEach(([key, item]) =>{
+        console.log(key + " " + val);
+        if(key === val){
+            console.log(item.value)
+        }
+    })
 }
+
+finddata("01")
